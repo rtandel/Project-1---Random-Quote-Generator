8f400157c4208fb4f2cebd420557d006283769f2
Rajen Tandel <rtandel124@gmail.com>
2016-04-08 16:29:57 -0400

    Resets!
    It displays all of the quotes before repeating them.

--- js/script.js
+++ js/script.js
@@ -13,50 +13,85 @@ var mQuotesAndSourcesArray = [
     quote : "Don't be concerned others not appreciating you."
             + " Be concerned about your not appreciating others.",
     source : "Confucius",
-    citation : "Cliches and one-liners Quotes"
+    citation : "Cliches and one-liners Quotes",
+    chosen : false
   },
   {
     quote : "You miss 100 percent of the shots you never take.",
-    source : "Wayne Getzky"
+    source : "Wayne Getzky",
+    chosen : false
   },
   {
     quote : "You must be the change you wish to see in the world.",
-    source : "Gandhi"
+    source : "Gandhi",
+    chosen : false
   },
   {
     quote : "It is not the strongest of the species that survives, nor the most "
           + "intelligent, but the one most responsive to change.",
-    source : "Leon C. Megginson"
+    source : "Leon C. Megginson",
+    chosen : false
   },
   {
     quote : "If you quit—quitting will become easier and easier for the rest of your life.",
-    source : "Osman Minkar"
+    source : "Osman Minkar",
+    chosen : false
   },
   {
     quote : "Growth means change and change involves risk, stepping from the known to the unknown.",
     source : "Benjamin Franklin",
-    citation : "Change Quotes"
+    citation : "Change Quotes",
+    chosen : false
   }
 ];
 
+// Array of colors to be chosen at random
 var mBackgroundColors = [
   "red",
   "green",
   "blue",
-  "yellow",
+  "dark-yellow",
   "grey",
   "purple"
 ]
 
 
+var counter = 0;           // Keeps track of which numbers were found or not.
 
+// Generates a random number to be used as the index and returns the object at
+// that index.
 function getRandomQuote() {
+
+  // Generates the Random number to serve as the index.
   mRandomNumber = Math.floor(Math.random() * mQuotesAndSourcesArray.length);
+
+  // This while loop makes sure that each quote is displayed once before
+  // repeating them.
+  while (mQuotesAndSourcesArray[mRandomNumber].chosen)  {
+      // reset all of the chosen properties to false if all quotes were displayed.
+      if (counter == mQuotesAndSourcesArray.length) {
+        // Loop to reset them all.
+        for (var a = 0; a < mQuotesAndSourcesArray.length; a++) {
+            mQuotesAndSourcesArray[a].chosen = false;
+            counter = 0;
+       }
+     }
+      mRandomNumber = Math.floor(Math.random() * mQuotesAndSourcesArray.length);
+  }
+
+  // Increment the counter for each array item that has true for it's chosen
+  // property.
+  counter++;
+  mQuotesAndSourcesArray[mRandomNumber].chosen = true;
+
   return mQuotesAndSourcesArray[mRandomNumber];
 }
 
-function printQuote() {
+setInterval(printQuote, 30000);   // Has it run automatically every 30 seconds.
 
+// Prints the new random quote
+function printQuote() {
+  // Choses and sets the background-color randomly when the button is pressed.
   document.body.style.backgroundColor
                     = mBackgroundColors[Math.floor(Math.random() * mBackgroundColors.length)];
 
@@ -80,6 +115,4 @@ function printQuote() {
                                                 + mSource + "</p>";
 
   }
-
-
 }
