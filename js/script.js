b5ddd229404761a1a73cb69d34df8cdd928b9d96
Rajen Tandel <rtandel124@gmail.com>
2016-04-08 11:29:13 -0400

    Refactoring the 2d array
    I replaced the 2d array with an array of objects.

--- js/script.js
+++ js/script.js
@@ -2,30 +2,84 @@
 // when user clicks anywhere on the page, the "makeQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
-var mRandomNumber;                    // The Random Number
-var mRandomQuote;                     // Random Quote
-var mRandomAuthor;                    // The random quote's author.
-// Two dimensional array to hold the quote and the person who said it.
+var mRandomNumber;              // The Random Number
+var mQuote;                     // Random Quote
+var mSource;                    // The random quote's author.
+var mCitation;                  // The citation
+var selectedObject;             // The object containing the new quote and source.
+// Array of quote objects
 var mQuotesAndSourcesArray = [
-  ["You can do anything, but not everything.", "David Allen" ],
-  ["You miss 100 percent of the shots you never take.", "Wayne Getzky" ],
-  ["You must be the change you wish to see in the world.", "Gandhi" ],
-  ["It is not the strongest of the species that survives, nor the most "
-  + "intelligent, but the one most responsive to change.", "Leon C. Megginson" ],
-  ["If you quit—quitting will become easier and easier for the rest of your life.", "Osman Minkar" ]
-
+  {
+    quote : "Don't be concerned others not appreciating you."
+            + " Be concerned about your not appreciating others.",
+    source : "Confucius",
+    citation : "Cliches and one-liners Quotes"
+  },
+  {
+    quote : "You miss 100 percent of the shots you never take.",
+    source : "Wayne Getzky"
+  },
+  {
+    quote : "You must be the change you wish to see in the world.",
+    source : "Gandhi"
+  },
+  {
+    quote : "It is not the strongest of the species that survives, nor the most "
+          + "intelligent, but the one most responsive to change.",
+    source : "Leon C. Megginson"
+  },
+  {
+    quote : "If you quit—quitting will become easier and easier for the rest of your life.",
+    source : "Osman Minkar"
+  },
+  {
+    quote : "Growth means change and change involves risk, stepping from the known to the unknown.",
+    source : "Benjamin Franklin",
+    citation : "Change Quotes"
+  }
 ];
+
+var mBackgroundColors = [
+  "red",
+  "green",
+  "blue",
+  "yellow",
+  "grey",
+  "purple"
+]
+
 
 
 function getRandomQuote() {
-  return Math.floor(Math.random() * mQuotesAndSourcesArray.length);
+  mRandomNumber = Math.floor(Math.random() * mQuotesAndSourcesArray.length);
+  return mQuotesAndSourcesArray[mRandomNumber];
 }
 
 function printQuote() {
-  mRandomNumber = getRandomQuote();
+
+  document.body.style.backgroundColor
+                    = mBackgroundColors[Math.floor(Math.random() * mBackgroundColors.length)];
+
+  selectedObject = getRandomQuote();
+  mQuote = selectedObject.quote;
+  mSource = selectedObject.source;
+  mCitation = selectedObject.citation;
   console.log(mRandomNumber);
-  document.getElementById("quote-box").innerHTML="<p class=\"quote\">"
-                                                + mQuotesAndSourcesArray[mRandomNumber][0] + "</p>"
+
+  // if there isn't a citation for a given quote, don't print one.
+  if (mCitation != null) {
+    document.getElementById("quote-box").innerHTML="<p class=\"quote\">"
+                                                + mQuote + "</p>"
                                                 + "<p class=\"source\">"
-                                                + mQuotesAndSourcesArray[mRandomNumber][1] + "</p>"
+                                                + mSource + "<span class=\"citation\">"
+                                                + mCitation + "</span></p>";
+  } else {
+      document.getElementById("quote-box").innerHTML="<p class=\"quote\">"
+                                                + mQuote + "</p>"
+                                                + "<p class=\"source\">"
+                                                + mSource + "</p>";
+
+  }
+
+
 }
